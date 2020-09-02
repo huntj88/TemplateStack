64706ef837dd793dd5617d17b8fae7b8a24ee0aa
huntj88 <huntj88@gmail.com>
2020-09-01 22:41:48 -0500

    spring start

---
+++ spring-template/src/test/kotlin/me/jameshunt/springtemplate/SpringTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.jameshunt.springtemplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringTemplateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
